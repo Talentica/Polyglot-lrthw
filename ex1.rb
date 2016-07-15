7b6cd55005597c962e6c050f9193e6fa1eb06832
Chetan Verma <verma.chetan@gmail.com>
2016-07-15 17:45:17 +0530

    Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+# Study Drills
+puts "Study Drill 1: This is another line as per the study drill!"
+puts "Study Drill 2: I didn't do it. Didn't want to delete all my effort ;)"
+puts "Study Drill 3: It just wanted to tell me that '#' is used for comments. I already put a comment above!!"
