6d84b5fffe04671af4ac4097b213782ab6cc41ab
Dextostone <vickykumar.3291@gmail.com>
2016-07-20 16:12:35 +0530

    Exercise 1 to 8

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello again."
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not."
+puts 'I "said" donot touch this.'
+# puts "This is a comment."
