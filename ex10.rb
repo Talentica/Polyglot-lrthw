c7000afb8bc0d7775743dbfb314a9167096cfa18
Dextostone <vickykumar.3291@gmail.com>
2016-07-22 14:29:05 +0530

    Exercise 9 and 10

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "\tI'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+i'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backlash_cat
+puts fat_cat
